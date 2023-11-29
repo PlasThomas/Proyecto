b71c2eddabe3e51c6f6d32141f4caa7c2dcbd5f5
PlasThomas <joseplascencia346@aragon.unam.mx>
2023-11-29 01:09:55 -0600

    funcion de actualizar precios- PLAS

--- programs/ActualizarP.php
+++ programs/ActualizarP.php
@@ -1,3 +1,16 @@
 <?php
+    include "Conexion.php";
+    $id = $_POST['Clave'];
+    $precio = $_POST['NPrecio'];
+
+    $q = "UPDATE productos set precio_ven = $precio where ID_productos = $id";
+    
+    if(mysqli_query($conexion,$q)){
+        echo "Precio modificado";
+        header("location: ../funcionesPags/Precios.php");
+    }else{
+        echo "ni modo";
+        header("location: ../funcionesPags/Precios.php");
+    }
 
 ?>
